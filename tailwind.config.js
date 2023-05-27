692ae4b53018785f083de9277274c3fb5662884e
aestas <hawhawba@gmail.com>
2023-05-28 01:11:01 +0900

    setup: tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts}', './src/**/*'],
+  theme: {
+    fontFamily: {
+      Pretendard: ['Pretendard'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
