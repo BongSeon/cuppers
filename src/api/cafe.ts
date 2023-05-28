73515a3939c552ce6d3706a780cf378ceccc29fd
aestas <hawhawba@gmail.com>
2023-05-28 23:19:28 +0900

    setup: port, axios, app.css, types, .env.base

---
+++ src/api/cafe.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const axiosState = {
+  baseURL: import.meta.env.VITE_BASE_API_URL,
+}
+console.log(axiosState.baseURL)
+const instance = axios.create(axiosState)
+
+const getCafe = async (cafe_id: number) => {
+  const apiUrl = `cafe/${cafe_id}`
+  const temp = await instance.get(apiUrl)
+
+  const res: FetchResponse = {
+    ok: temp.status === 200,
+    status: temp.status,
+    data: temp.data,
+  }
+  return res
+}
+
+const useCafeAPI = {
+  getCafe,
+}
+export default useCafeAPI
---
+++ src/types/general.d.ts
@@ -0,0 +1,7 @@
+interface FetchResponse {
+  readonly ok: boolean
+  readonly status?: number
+  readonly statusText?: string
+  readonly data?: any
+  readonly reason?: any
+}
