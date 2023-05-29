0ab79fae3af17dea98fa3e3190f1ab2fe5dcea5b
aestas <hawhawba@gmail.com>
2023-05-29 23:52:34 +0900

    make up: base css, tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts}', './src/**/*'],
   theme: {
-    fontFamily: {
-      Pretendard: ['Pretendard'],
+    // fontFamily: {
+    //   Pretendard: ['Pretendard'],
+    // },
+    extend: {
+      colors: {
+        primary: '#674a40',
+        secondary: '#8cadbe',
+        positive: '#51CE6E',
+        negative: '#ED6954',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
