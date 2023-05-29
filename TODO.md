0dad81ff1d70c8c3670b4088c5c11f8c2e9101e3
aestas <hawhawba@gmail.com>
2023-05-29 21:52:29 +0900

    setup: unplugin-auto-import

---
+++ TODO.md
@@ -0,0 +1,13 @@
+# Project History and TODO
+
+## 5/28
+
+- init
+- tailwind, port, axios, app.css, types
+- .env
+- add basic layout, navbar
+
+## 5/29
+
+- [x] add iconify/vue, general component
+- [x] setup: unplugin-auto-import
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      include: [/\.[tj]sx?$/, /\.vue$/],
+      imports: ['vue', 'vue-router', 'pinia'],
+      dts: true, // Autoimport all the files that ends with d.ts
+      vueTemplate: true, // Autoimport inside vue template
+      eslintrc: { enabled: true },
+    }),
+  ],
   server: {
     port: 8080,
   },
